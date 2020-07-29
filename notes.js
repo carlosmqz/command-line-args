eb676b19774f170a95e85ae5cc27fe3740016f34
carlosmqz <carlosmqz@gmail.com>
2020-07-29 13:08:13 -0500

    add method fixed to work async

--- notes.js
+++ notes.js
@@ -5,33 +5,28 @@ const getNotes = function(){
     return 'Your notes...'
 }
 const addNote = function(title, body){
-    const notes =  loadNotes()
-    console.log(notes)
-     /*notes.push({
-         title:title,
-         body:body
-    })*/
-}
-const loadNotes = function(){
-
-    //const read = util.promisify(fs.readFile);
-    const openFile = util.promisify(fs.open);
-    try{
-        const dataBuffer = function(){
-            return openFile('notes.json','a+')
-        }
-        let jsonData=''
-        dataBuffer().then(data => {
-            jsonData = data.toString();
-        })
-        console.log(jsonData)
-        const notesData = JSON.stringify(jsonData) 
-        return notesData
-    }catch(e){
-        console.error(e)
-    }
+    const notes = 
+        fs.promises.readFile('notes.json',{'flag':'r+'}).then((data) => {
+            const jsonData = data.length > 0 ? JSON.parse(Buffer.from(data).toString()): []
+            jsonData.push({
+                title:title,
+                body:body
+            })
+            return {
+                    file: 'notes.json',
+                    data:jsonData
+                }
+         }).then((dataObject) => {
+            fs.promises.writeFile(dataObject.file,JSON.stringify(dataObject.data))
+                .then(() => {
+                    console.log('Data saved successfully!')
+                    return dataObject.file
+                }).catch((e) => {console.log('Error: '+ e)})
+            
+        }).catch((e) => {console.log(e)})
     
     
+    //console.log(notes)
 }
 
 
